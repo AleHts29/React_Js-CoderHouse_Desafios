3f1fdcabf63cd009ac061f7ec6020030edd8e889
AleHts29 <ahuertas@fi.uba.ar>
2021-06-22 00:54:32 -0300

    Desafio5

--- clase5-app/src/componets/navbarComponents/CardWidget.jsx
+++ clase5-app/src/componets/navbarComponents/CardWidget.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import {FiShoppingCart} from 'react-icons/fi'
 
+import * as ReactBootStrap from 'react-bootstrap'
+
 const CardWidget = () => {
     return (
-        <div className="text-white" size="lg">
-           < FiShoppingCart/>
+        <div className="text-white d-flex mr-5 mt-1" size="lg">
+        <div><FiShoppingCart/></div>         
+        <div>
+        <ReactBootStrap.NavDropdown FiShoppingCart id="collasible-ReactBootStrap.Nav-dropdown">
+            <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown.Divider />
+            <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
+           <div>
+           
+            <td>
+                <button class="btn btn-warning btn-up btn-sm ml-5">
+                    +
+                </button>
+                <button class="btn btn-warning btn-down btn-sm ml-1">
+                    -
+                </button>
+            </td> 
+           
+           </div>
+       
+
+        </ReactBootStrap.NavDropdown> 
+      </div>
+      
         </div>
     )
 }
 
 export default CardWidget
+
+
+
--- clase5-app/src/componets/navbarComponents/Navbar.js
+++ clase5-app/src/componets/navbarComponents/Navbar.js
@@ -6,31 +6,36 @@ const Navbar = () => {
 
 <>
 
-     <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+<ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <ReactBootStrap.Navbar.Brand href="#home">Logo</ReactBootStrap.Navbar.Brand>
   <ReactBootStrap.Navbar.Toggle aria-controls="responsive-ReactBootStrap.Navbar-ReactBootStrap.Nav" />
   <ReactBootStrap.Navbar.Collapse id="responsive-ReactBootStrap.Navbar-ReactBootStrap.Nav">
     <ReactBootStrap.Nav className="mr-auto">
       <ReactBootStrap.Nav.Link href="#features">Home</ReactBootStrap.Nav.Link>
       <ReactBootStrap.Nav.Link href="#pricing">Items</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-ReactBootStrap.Nav-dropdown">
+      <div>< CardWidget /></div>
+      
+      {/* <ReactBootStrap.NavDropdown  id="collasible-ReactBootStrap.Nav-dropdown">
         <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
         <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
         <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
         <ReactBootStrap.NavDropdown.Divider />
         <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
-      </ReactBootStrap.NavDropdown>
+      </ReactBootStrap.NavDropdown> */}
     </ReactBootStrap.Nav>
 
     {/* Icono cart */}
-    <ReactBootStrap.Nav>
+    {/* <ReactBootStrap.Nav>
       <ReactBootStrap.Nav.Link href="#deets">
-        <CardWidget/>
+        <CardWidget />
       </ReactBootStrap.Nav.Link>  
-    </ReactBootStrap.Nav>
+    </ReactBootStrap.Nav> */}
+
 
   </ReactBootStrap.Navbar.Collapse>
 </ReactBootStrap.Navbar>
+
+
 
 
 </>
